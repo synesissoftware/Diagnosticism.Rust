2c5cac9d122a252f46ceb817f8010b525fc22aa9
Matt Wilson <152443343+mwsis@users.noreply.github.com>
2024-09-13 15:01:24 +1000

    added `DebugSqueezer`, `Ellipsis`, `Password` (#1)
    * Merged 'ellipsis' into 'master'
    
    Squashed commit of the following:
    
    commit c54ca626a3d9acba294ae8689de06e395734f823
    Author: Matt Wilson <matthew.wilson.synesis@gmail.com>
    Date:   Fri Sep 13 07:50:44 2024 +1000
    
        + added `diagnostics::Ellipsis`
    
    * Merged 'debug_squeezer' into 'master'
    
    Squashed commit of the following:
    
    commit f0cdc042e6d2815d51d48ff08bb23807652926c8
    Author: Matt Wilson <matthew.wilson.synesis@gmail.com>
    Date:   Fri Sep 13 12:13:07 2024 +1000
    
        + added `diagnostics::DebugSqueezer`
    
    * Merged 'password' into 'master'
    
    Squashed commit of the following:
    
    commit aa83421a3429ba048566372b5347c3530304d087
    Author: Matt Wilson <matthew.wilson.synesis@gmail.com>
    Date:   Fri Sep 13 13:57:30 2024 +1000
    
        + added `diagnostics::Password`
    
    ---------
    
    Co-authored-by: synesissoftware <matthew@synesis.com.au>

---
+++ examples/debug_squeezer.rs
@@ -0,0 +1,216 @@
+// examples/debug_squeezer.rs
+
+#![allow(dead_code)]
+
+// Illustrates the use of [`DebugSqueezer`].
+
+use diagnosticism::diagnostics::{
+    DebugSqueezer,
+};
+
+use std::{
+    collections::{
+        BTreeMap,
+        HashMap,
+    },
+    fmt as std_fmt,
+};
+
+
+type NestedMap = BTreeMap<i32, HashMap<i32, i32>>;
+
+fn make_maps() -> (
+    BTreeMap<i32, NestedMap>, // btm
+    HashMap<i32, NestedMap>,  // hm
+) {
+    let mut btm = BTreeMap::new();
+    let mut hm = HashMap::new();
+
+    btm.insert(
+        0,
+        NestedMap::from([
+            // insert list:
+            (
+                1,
+                HashMap::from([
+                    // insert list:
+                    (2, 3),
+                    (4, 5),
+                ]),
+            ),
+            (
+                6,
+                HashMap::from([
+                    // insert list:
+                    (7, 8),
+                    (9, 10),
+                ]),
+            ),
+        ]),
+    );
+    hm.insert(
+        11,
+        NestedMap::from([
+            // insert list:
+            (
+                12,
+                HashMap::from([
+                    // insert list:
+                    (13, 14),
+                    (15, 16),
+                ]),
+            ),
+            (
+                17,
+                HashMap::from([
+                    // insert list:
+                    (18, 19),
+                    (20, 21),
+                ]),
+            ),
+        ]),
+    );
+
+    (btm, hm)
+}
+
+#[derive(Debug)]
+struct WithoutSqueezer {
+    btm : BTreeMap<i32, NestedMap>,
+    hm :  HashMap<i32, NestedMap>,
+}
+
+impl WithoutSqueezer {
+    pub fn new() -> Self {
+        let (btm, hm) = make_maps();
+
+        Self {
+            btm,
+            hm,
+        }
+    }
+}
+
+#[derive(Default)]
+struct WithSqueezer {
+    btm :           BTreeMap<i32, NestedMap>,
+    hm :            HashMap<i32, NestedMap>,
+    squeeze_width : usize,
+}
+
+impl WithSqueezer {
+    pub fn new(squeeze_width : usize) -> Self {
+        let (btm, hm) = make_maps();
+
+        Self {
+            btm,
+            hm,
+            squeeze_width,
+        }
+    }
+}
+
+/*
+fn touch_formatter<'a>(
+    f: &std_fmt::Formatter<'_>,
+) -> &'static str {
+    ""
+}
+ */
+
+impl std_fmt::Debug for WithSqueezer {
+    fn fmt(
+        &self,
+        f : &mut std_fmt::Formatter<'_>,
+    ) -> std_fmt::Result {
+        f.debug_struct("WithSqueezer")
+            .field(
+                "btm",
+                &DebugSqueezer::new(&self.btm, self.squeeze_width),
+            )
+            .field(
+                "hm",
+                &DebugSqueezer::new(&self.hm, self.squeeze_width),
+            )
+            .finish()
+    }
+}
+
+
+fn main() {
+    {
+        let wo_sqz = WithoutSqueezer::new();
+
+        println!("`Debug` form of `WithoutSqueezer` is '{wo_sqz:?}'");
+    }
+
+    {
+        let squeeze_width = 50;
+        let w_sqz = WithSqueezer::new(squeeze_width);
+
+        println!("`Debug` form of `WithSqueezer` ({squeeze_width}) is '{w_sqz:?}'");
+    }
+
+    {
+        let squeeze_width = 20;
+        let w_sqz = WithSqueezer::new(squeeze_width);
+
+        println!("`Debug` form of `WithSqueezer` ({squeeze_width}) is '{w_sqz:?}'");
+    }
+
+    {
+        let squeeze_width = 10;
+        let w_sqz = WithSqueezer::new(squeeze_width);
+
+        println!("`Debug` form of `WithSqueezer` ({squeeze_width}) is '{w_sqz:?}'");
+    }
+
+    {
+        let squeeze_width = 8;
+        let w_sqz = WithSqueezer::new(squeeze_width);
+
+        println!("`Debug` form of `WithSqueezer` ({squeeze_width}) is '{w_sqz:?}'");
+    }
+
+    {
+        let squeeze_width = 7;
+        let w_sqz = WithSqueezer::new(squeeze_width);
+
+        println!("`Debug` form of `WithSqueezer` ({squeeze_width}) is '{w_sqz:?}'");
+    }
+
+    {
+        let squeeze_width = 6;
+        let w_sqz = WithSqueezer::new(squeeze_width);
+
+        println!("`Debug` form of `WithSqueezer` ({squeeze_width}) is '{w_sqz:?}'");
+    }
+
+    {
+        let squeeze_width = 5;
+        let w_sqz = WithSqueezer::new(squeeze_width);
+
+        println!("`Debug` form of `WithSqueezer` ({squeeze_width}) is '{w_sqz:?}'");
+    }
+
+    {
+        let squeeze_width = 4;
+        let w_sqz = WithSqueezer::new(squeeze_width);
+
+        println!("`Debug` form of `WithSqueezer` ({squeeze_width}) is '{w_sqz:?}'");
+    }
+
+    {
+        let squeeze_width = 3;
+        let w_sqz = WithSqueezer::new(squeeze_width);
+
+        println!("`Debug` form of `WithSqueezer` ({squeeze_width}) is '{w_sqz:?}'");
+    }
+
+    {
+        let squeeze_width = 2;
+        let w_sqz = WithSqueezer::new(squeeze_width);
+
+        println!("`Debug` form of `WithSqueezer` ({squeeze_width}) is '{w_sqz:?}'");
+    }
+}
---
+++ examples/ellipsis.rs
@@ -0,0 +1,7 @@
+use diagnosticism::diagnostics::Ellipsis;
+
+fn main() {
+    let ellipsis = Ellipsis::default();
+
+    println!("`Debug` form of `Ellipsis` is '{ellipsis:?}'");
+}
