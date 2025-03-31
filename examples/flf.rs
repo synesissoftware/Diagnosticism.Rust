058910fead0a0560e29cece544883bdee6c2d236
Matt Wilson <152443343+mwsis@users.noreply.github.com>
2025-03-31 19:26:03 +1100

    Added file+line+function macros (initial implementations) (#5)
    * + added file+line+function macros (initial implementations)
    
    * ~ fixing extra-crate dependencies
    
    * ~ simplifying (and removing `unsafe`)

---
+++ examples/flf.rs
@@ -0,0 +1,64 @@
+use diagnosticism::{
+    fileline,
+    filelinefunction,
+    filelinefunction_fully_qualified_name as filelinefunction_fqn,
+    function_fully_qualified_name,
+    function_name_only,
+};
+
+fn fl() {
+    eprintln!("{}", file!());
+}
+
+fn fl_ln() {
+    eprintln!("{}", fileline!());
+}
+
+fn fun() {
+    eprintln!("{}", function_name_only!());
+}
+
+fn fun_fqn() {
+    eprintln!("{}", function_fully_qualified_name!());
+}
+
+fn fl_ln_fun() {
+    eprintln!("{}", filelinefunction!());
+}
+
+fn fl_ln_fun_fqn() {
+    eprintln!("{}", filelinefunction_fqn!());
+}
+
+fn main() {
+
+    eprintln!("");
+    eprintln!("calling `fl()`:");
+
+    fl();
+
+    eprintln!("");
+    eprintln!("calling `fun()`:");
+
+    fun();
+
+    eprintln!("");
+    eprintln!("calling `fun_fqn()`:");
+
+    fun_fqn();
+
+    eprintln!("");
+    eprintln!("calling `fl_ln()`:");
+
+    fl_ln();
+
+    eprintln!("");
+    eprintln!("calling `fl_ln_fun()`:");
+
+    fl_ln_fun();
+
+    eprintln!("");
+    eprintln!("calling `fl_ln_fun_fqn()()`:");
+
+    fl_ln_fun_fqn();
+}
---
+++ src/diagnostics/flf.rs
@@ -0,0 +1,116 @@
+// src/diagnostics/flf.rs : Defines file+line+function macros
+
+/// Expands to the file name and line number in which it was invoked.
+///
+/// As with the standard [`file!`], [`line!`], and [`column!`] macros, these
+/// macros provide debugging information for developers about location
+/// within the source.
+///
+/// # Examples
+///
+/// ```
+/// let this_file_line = diagnosticism::fileline!();
+/// println!("defined at this file/line: {this_file_line}");
+/// ```
+#[macro_export]
+macro_rules! fileline {
+    () => {
+        concat!(file!(), ":", line!())
+    }
+}
+
+/// T.B.C.
+#[macro_export]
+macro_rules! function_fully_qualified_name {
+    () => {{
+        fn f() {}
+        fn type_name_of<T>(_: T) -> &'static str {
+            std::any::type_name::<T>()
+        }
+
+        let name : &'static str = type_name_of(f);
+
+        let name = if name.ends_with("::f") {
+            &name[..name.len() - 3]
+        } else {
+            name
+        };
+
+        name
+    }};
+}
+
+/// T.B.C.
+#[macro_export]
+macro_rules! function_name_only {
+    () => {{
+        fn f() {}
+        fn type_name_of<T>(_: T) -> &'static str {
+            std::any::type_name::<T>()
+        }
+
+        let name : &'static str = type_name_of(f);
+
+        let name = if name.ends_with("::f") {
+            &name[..name.len() - 3]
+        } else {
+            name
+        };
+
+        match &name[..name.len()].rfind(':') {
+            Some(pos) => &name[pos + 1..name.len()],
+            None => &name[..name.len()],
+        }
+    }};
+}
+
+/// Expands to the file name and line number and function name in which it
+/// was invoked.
+///
+/// As with the standard [`file!`], [`line!`], and [`column!`] macros, these
+/// macros provide debugging information for developers about location
+/// within the source.
+///
+/// # Examples
+///
+/// ```
+/// let this_file_line_fn = diagnosticism::filelinefunction!();
+/// println!("defined at this file/line/function: {this_file_line_fn}");
+/// ```
+#[macro_export]
+macro_rules! filelinefunction {
+    () => {{
+        format!("{}:{}", $crate::fileline!(), $crate::function_name_only!())
+    }}
+}
+
+/// T.B.C.
+#[macro_export]
+macro_rules! filelinefunction_fully_qualified_name {
+    () => {{
+        format!("{}:{}", $crate::fileline!(), $crate::function_fully_qualified_name!())
+    }}
+}
+
+
+#[cfg(test)]
+mod tests {
+    #![allow(non_snake_case)]
+
+
+    #[test]
+    fn TEST_fileline() {
+        let expected = format!("{}:{}", file!(), (line!() + 1));
+        let actual = fileline!();
+
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn TEST_filelinefunction() {
+        let expected = format!("{}:{}:{}", file!(), (line!() + 1), "TEST_filelinefunction");
+        let actual = filelinefunction!();
+
+        assert_eq!(expected, actual);
+    }
+}
