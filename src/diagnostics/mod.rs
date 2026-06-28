a5e1a1c36f80bf7ad1ab85ab08375025e28303d1
Matt Wilson <matthew@synesis.com.au>
2026-06-28 21:20:41 +1000

    chore: internal implementation improvements

--- src/diagnostics/mod.rs
+++ src/diagnostics/mod.rs
@@ -1,14 +1,6 @@
 // diagnostics/mod.rs
 
-macro_rules! declare_and_publish {
-    ($mod_name:ident, $($type_name:ident),* $(,)?) => {
-        mod $mod_name;
-
-        pub use $mod_name::{
-            $($type_name),*
-        };
-    };
-}
+use crate::macros::declare_and_publish;
 
 declare_and_publish!(debug_squeezer, DebugSqueezer);
 declare_and_publish!(doomgram, DoomGram, doom_scope);
